46ff1af11a5e2e7d2b65bd6bb13bd2c8608f1c4f
Scott Griv <scottgrivner@gmail.com>
2024-02-08 19:39:16 -0500

    Moved files

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "../user-management-api.yaml",
+    url: "./user-management-api.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
